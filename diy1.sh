db953457988b1868b97a79c2453b1ffc47716557
Suct41 <73008240+Suct41@users.noreply.github.com>
2022-07-17 15:19:02 +0800

    Update diy1.sh

--- diy1.sh
+++ diy1.sh
@@ -10,16 +10,6 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git jerryk https://github.com/jerrykuku/openwrt-package' >>feeds.conf.default
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-# delete package（LEDE）
-find . -name 'adguardhome' -o -name 'adguardhome' | xargs -i rm -rf {}
-find . -name 'luci-app-adguardhome' -o -name 'adguardhome' | xargs -i rm -rf {}
