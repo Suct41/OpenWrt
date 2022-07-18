0d590cca2d08b714f0ce7a17d738778d1ad3435f
Suct41 <73008240+Suct41@users.noreply.github.com>
2022-07-18 16:56:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,8 +13,20 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# Modify hostname
-sed -i 's/OpenWrt/Suct41/g' package/base-files/files/bin/config_generate
-
 # 删除自带adguardhome
 rm -rf feeds/packages/net/adguardhome
+
+# 禁止解析 IPv6 DNS 记录
+sed -i '/dnsmasq/aoption filter_aaaa 1'  package/network/services/dnsmasq/files/dhcp.conf
+
+#不记录日志
+sed -i '/dnsmasq/aoption quietdhcp 1' package/network/services/dnsmasq/files/dhcp.conf
+
+# 禁用内置的 IPv6 管理， /etc/config/network 中 config interface 'wan'、config interface 'lan' 字段下
+sed -i "/proto='none'/aset network.\$1.delegate='0'"  package/base-files/files/bin/config_generate
+
+# Modify hostname
+sed -i 's/OpenWrt/suct41/g' package/base-files/files/bin/config_generate 
+
+#修正连接数（by ベ七秒鱼ベ）
+sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
