e247a9f878746ab58ddf886c0131abf6af78791c
Suct41 <73008240+Suct41@users.noreply.github.com>
2022-07-19 22:18:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,10 +9,6 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 
-# adguarddhome
-rm -rf feeds/packages/net/adguarddhome
-# bootstrap
-rm -rf package/lean/luci-theme-bootstrap
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
